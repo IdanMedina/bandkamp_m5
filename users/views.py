d97ce632551718a7b0c6e16d18eb35b6ad3eca09
Idan <mquinhoz@hotmail.com>
2023-04-19 01:27:18 -0300

    feat: task 2 finished

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView, Request, Response, status
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
@@ -6,22 +7,19 @@ from django.shortcuts import get_object_or_404
 from .permissions import IsAccountOwner
 
 
-class UserView(APIView):
-    def post(self, request: Request) -> Response:
-        """
-        Registro de usuários
-        """
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        serializer.save()
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+class UserView(CreateAPIView):
+    """
+    Registro de usuários
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserDetailView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
     def get(self, request: Request, pk: int) -> Response:
         """
